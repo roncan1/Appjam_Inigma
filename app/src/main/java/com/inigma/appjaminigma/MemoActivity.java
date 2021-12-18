75a213712937251564bd0b3e893aba527ee97a63
Taeh83 <dreamsky83@naver.com>
2021-12-18 17:54:20 +0900

    fourth view add

--- app/src/main/java/com/inigma/appjaminigma/MemoActivity.java
+++ app/src/main/java/com/inigma/appjaminigma/MemoActivity.java
@@ -1,14 +1,23 @@
 package com.inigma.appjaminigma;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MemoActivity extends AppCompatActivity {
 
+    TextView titleText; //주제 textView
+    EditText customText; //설명 editText
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_memo);
+        setContentView(R.layout.activity_memo); //출
+
+        //텍스트뷰에 텍스트가 출력되게끔하기
+        titleText = (TextView)findViewById(R.id.showText);
+        customText = (EditText)findViewById(R.id.makeText);
     }
 }
