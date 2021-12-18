ad5d3bc1ee1f421c3ea3ed5184f39142d372679a
roncan <65841016+roncan1@users.noreply.github.com>
2021-12-19 02:22:09 +0900

    memoAvtivity Base

--- app/src/main/java/com/inigma/appjaminigma/MemoActivity.java
+++ app/src/main/java/com/inigma/appjaminigma/MemoActivity.java
@@ -8,16 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MemoActivity extends AppCompatActivity {
 
-    TextView titleText; //주제 textView
-    EditText customText; //설명 editText
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo); //출
 
-        //텍스트뷰에 텍스트가 출력되게끔하기
-        titleText = (TextView)findViewById(R.id.showText);
-        customText = (EditText)findViewById(R.id.makeText);
     }
 }
