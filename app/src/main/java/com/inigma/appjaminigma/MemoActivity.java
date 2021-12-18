2d6fbf179c2b7692cfd32f6f5a8365dd98fe8ee5
roncan <65841016+roncan1@users.noreply.github.com>
2021-12-18 14:30:50 +0900

    activity_memo.xml activity_mindmap.xml 추가, 상단 액션바 제거

---
+++ app/src/main/java/com/inigma/appjaminigma/MemoActivity.java
@@ -0,0 +1,14 @@
+package com.inigma.appjaminigma;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo);
+    }
+}
